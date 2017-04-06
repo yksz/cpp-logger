11beae99eafcb134fa72ec4a952a1d0dc07203d1
yksz <yksz.dev@gmail.com>
2017-04-06 22:05:48 +0900

    optimize: Decrease the number of notify_one calls

--- src/log_queue.h
+++ src/log_queue.h
@@ -17,23 +17,29 @@ public:
 
     void Push(T&& element) {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while (m_queue.size() + 1 > m_capacity) { // is full
+        while (isFull()) {
             m_notfull.wait(lock);
         }
+        bool wasEmpty = isEmpty();
         m_queue.push(std::move(element));
-        m_notempty.notify_one();
+        if (wasEmpty) {
+            m_notempty.notify_one();
+        }
     }
 
     void Pop(T* element) {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while (m_queue.empty()) { // is empty
+        while (isEmpty()) {
             m_notempty.wait(lock);
         }
+        bool wasFull = isFull();
         if (element != nullptr) {
             *element = std::move(m_queue.front());
         }
         m_queue.pop();
-        m_notfull.notify_one();
+        if (wasFull) {
+            m_notfull.notify_one();
+        }
     }
 
 private:
@@ -42,6 +48,14 @@ private:
     std::mutex m_mutex;
     std::condition_variable m_notfull;
     std::condition_variable m_notempty;
+
+    bool isEmpty() {
+        return m_queue.empty();
+    }
+
+    bool isFull() {
+        return m_queue.size() + 1 > m_capacity;
+    }
 };
 
 } // namespace log
